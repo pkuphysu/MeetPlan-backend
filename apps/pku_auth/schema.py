3ee25a9cbb44704c351f2f2d105ba08212ff6107
Rainshaw <rxg@live.com>
2021-04-15 01:40:22 +0800

    docs: add docs for AbstractMeta class

--- apps/pku_auth/schema.py
+++ apps/pku_auth/schema.py
@@ -69,6 +69,50 @@ class ObtainJSONWebToken(ObtainJSONWebTokenMixin, graphene.Mutation):
 
 
 class AbstractMeta:
+    """
+    An abstract meta class for graphene_django_plus.types.ModelType.
+
+    **Extra options**
+
+    ``AbstractMeta.interfaces``
+
+        *Default*: ``(graphene.relay.Node)``
+
+    ``AbstractMeta.connection_class``
+
+        *Default*: ``graphene_django_plus.fields.CountableConnection``
+
+        Connection that provides a total_count attribute.
+
+    ``AbstractMeta.allow_unauthenticated``
+
+        *Default*: ``False``
+
+        If unauthenticated users should be allowed to retrieve any object
+        of this type. This is not dependant on `GuardedModel` and neither
+        `guardian` and is defined as `False` by default.
+
+    ``AbstractMeta.permissions``
+
+        *Default*: ``[]``
+
+        A list of Django model permissions to check. Different from
+        object_permissions, this uses the basic Django's permission system
+        and thus is not dependant on `GuardedModel` and neither `guardian`.
+        This is an empty list by default.
+
+    ``AbstractMeta.object_permissions``
+
+        *Default*: ``[]``
+
+        When adding this to a query, only objects with a `can_read`
+        permission to the request's user will be allowed to return to him
+        Note that `can_read` was defined in the model.
+        If the model doesn't inherid from `GuardedModel`, `guardian` is not
+        installed ot this list is empty, any object will be allowed.
+        This is empty by default.
+
+    """
     class Meta:
         abstract = True
 
