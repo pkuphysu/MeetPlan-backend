733b61079299f72e6cdab116ed505f7f3405cea3
Rainshaw <rxg@live.com>
2021-04-17 01:41:25 +0800

    feat: add base framework files of django

---
+++ MeetPlan/schema.py
@@ -0,0 +1,27 @@
+import graphene
+from django.conf import settings
+from graphene_django.debug import DjangoDebug
+
+# import apps.meet_plan.schema
+import apps.pku_auth.schema
+# import apps.user.schema
+
+
+class Query(
+    # apps.meet_plan.schema.Query,
+    # apps.user.schema.Query,
+    apps.pku_auth.schema.Query,
+    graphene.ObjectType):
+    if settings.DEBUG:
+        debug = graphene.Field(DjangoDebug, name='_debug')
+
+
+class Mutation(
+    # apps.meet_plan.schema.Mutation,
+    # apps.user.schema.Mutation,
+    apps.pku_auth.schema.Mutation,
+    graphene.ObjectType):
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
